596970ae13d518cee883cb98e8529832307af6a9
reen967 <50892512+reen967@users.noreply.github.com>
2024-01-30 13:23:57 +0000

    Update gen-rss.js

--- scripts/gen-rss.js
+++ scripts/gen-rss.js
@@ -5,9 +5,9 @@ const matter = require('gray-matter')
 
 async function generate() {
   const feed = new RSS({
-    title: 'Your Name',
-    site_url: 'https://yoursite.com',
-    feed_url: 'https://yoursite.com/feed.xml'
+    title: 'Arina Berezovsky',
+    site_url: 'https://arinaberezovsky.com',
+    feed_url: 'https://arinaberezovsky.com/feed.xml'
   })
 
   const posts = await fs.readdir(path.join(__dirname, '..', 'pages', 'posts'))
